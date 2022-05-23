1ad1e2163fd0659470a74b6d7eed980ede517900
Debjoty Mitra <debjotyms@gmail.com>
2022-05-23 11:38:25 +0600

    Imported class from another folder.

---
+++ BookClassTest.py
@@ -0,0 +1,8 @@
+from Classes import Book
+
+book1 = Book.Book("Opekkha", "Humayun Ahmed")
+book1.set_price(250)
+book1.view()
+print(book1.get_price())
+
+
---
+++ Classes/Book.py
@@ -0,0 +1,15 @@
+class Book:
+
+    def __init__(self, name, author):
+        self.name = name
+        self.author = author
+        self.price = 0
+
+    def set_price(self, price):
+        self.price = price
+
+    def get_price(self):
+        return self.price
+
+    def view(self):
+        print(self.name, "by", self.author, self.price)
